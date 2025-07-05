4c3a46261f6af96279a63ed83ec1c93ad69a4594
姚欢 <yaohuan@MacBook-Air.local>
2025-07-05 21:05:51 +0800

    aaa

--- 5. python基础/5.1数据类型和变量.py
+++ 5. python基础/5.1数据类型和变量.py
@@ -1,4 +1,14 @@
-big_number = 10_0_0_0_000_000
-print(big_number)
-hex = 0xa1b2_c3d4
-print(hex)
+def func():
+    print("a")
+    yield
+    print("b")
+    yield
+    print("c")
+
+def a():
+    co = func()
+    next(co)
+    print('in function A')
+    next(co)
+
+a()
