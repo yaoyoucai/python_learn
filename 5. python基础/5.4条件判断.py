142a19c8489ddaaec296bae621b1e3d994495fec
yaohuan <liwinallucky@163.com>
2025-07-18 15:59:07 +0800

    增加条件判断

---
+++ 5. python基础/5.4条件判断.py
@@ -0,0 +1,14 @@
+# if 语句
+def test_if():
+    age = 45
+    if age <= 18:
+        print('your age is ',age)
+        print('adult')
+    elif age >18 and age < 50:
+        print('your age is ',age)
+        print('lbd')
+    else:
+        print('your age is ',age)
+        print('llllbd')      
+
+test_if()
---
+++ 7.高级特性/7.1.打印列表.py
@@ -0,0 +1,6 @@
+L = []
+value = 1
+while value < 99:
+    L.append(value)
+    value = value +2
+    print(value)
